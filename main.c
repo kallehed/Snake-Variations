d82017ecfb0b173f92e113ceb06f358e9cbca633
kallehed <kallehed@gmail.com>
2023-06-24 16:40:35 +0200

    finish maze level, make snakes move and hurt you

--- main.c
+++ main.c
@@ -183,7 +183,7 @@ static void player_draw_extra(const Player *player, const World_State0 *w)
 }
 
 // NOT PURE
-static Dir get_dir_from_input()
+static Dir get_dir_from_input(void)
 {
     if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_L))
     {
@@ -350,33 +350,25 @@ static void evil_snake_move(Evil_Snake *snake, World_State0 *w)
     }
 }
 
-static void evil_snakes_draw(const Evil_Snake snakes[], const Int length, const World_State0 *w)
+static void evil_snake_draw(const Evil_Snake *snake, const World_State0 *w)
 {
-    for (Int i = 0; i < length; ++i)
+    const Int length = snake->length;
+    for (Int j = 0; j < length; ++j)
     {
-        const Evil_Snake *snake = &snakes[i];
-        for (Int j = 0; j < snake->length; ++j)
-        {
-            draw_block_at(snake->positions[j], (j == 0) ? SKYBLUE : BLUE, w);
-        }
+        draw_block_at(snake->positions[j], (j == 0) ? SKYBLUE : BLUE, w);
     }
 }
 
 // returns true on collision
-static bool evil_snakes_player_collision_logic(const Evil_Snake snakes[], const Int length, const Player *player)
+static bool evil_snake_player_collision_logic(const Evil_Snake *snake, const Player *player)
 {
-    for (Int snake_idx = 0; snake_idx < length; ++snake_idx)
+    for (int i = 0; i < player->length; ++i)
     {
-        const Evil_Snake *snake = &snakes[snake_idx];
-
-        for (int i = 0; i < 1; ++i) // only collide with the head
+        for (int j = 0; j < snake->length; ++j)
         {
-            for (int j = 0; j < snake->length; ++j)
+            if (pos_equal(player_nth_position(player, i), snake->positions[j]))
             {
-                if (pos_equal(player_nth_position(player, i), snake->positions[j]))
-                {
-                    return true;
-                }
+                return true;
             }
         }
     }
@@ -566,7 +558,7 @@ static void maze_draw(const Maze_Cell maze[], const Int maze_width, const Int ma
             }
             break;
             case Maze_Cell_Wall: {
-                col = BLACK;
+                col = (Color){0, 0, 0, 200};
             }
             break;
             }
@@ -575,15 +567,86 @@ static void maze_draw(const Maze_Cell maze[], const Int maze_width, const Int ma
     }
 }
 
+typedef struct
+{
+    Dir dir;
+    Coord len;
+} Snake_Pather_Way;
+
+#define SNAKE_PATHER_MAX_WAYS 20
+#define SNAKE_PATHER_MAX_LEN 10
+typedef struct
+{
+    Pos positions[SNAKE_PATHER_MAX_LEN];
+    Coord len;
+    // ways to go in path
+    Snake_Pather_Way ways[SNAKE_PATHER_MAX_WAYS];
+    // CONST. how many actual ways you have
+    Int ways_len;
+    // what way are we currently on ^
+    Int way_idx;
+    // Starts high, goes to zero, depending on Way::len
+    Int walk_this_way_counter;
+} Snake_Pather;
+
+static void snake_pather_draw(Snake_Pather *snake_pather, World_State0 *w)
+{
+    for (Int i = 0; i < snake_pather->len; ++i)
+    {
+        draw_block_at(snake_pather->positions[i], BLUE, w);
+    }
+}
+
+static void snake_pather_move(Snake_Pather *snake_pather, World_State0 *w)
+{
+    // get new way
+    printf("path move counter: %d\n", snake_pather->walk_this_way_counter);
+    if (0 >= snake_pather->walk_this_way_counter)
+    {
+        snake_pather->way_idx++;
+        // we have gone all the ways, return to first again
+        if (snake_pather->way_idx >= snake_pather->ways_len)
+        {
+            snake_pather->way_idx = 0;
+        }
+        snake_pather->walk_this_way_counter = snake_pather->ways[snake_pather->way_idx].len;
+    }
+
+    // evil_snake_move(&snake_path->snake, w);
+    Dir dir = snake_pather->ways[snake_pather->way_idx].dir;
+    for (Int i = snake_pather->len - 1; i >= 1; --i)
+    {
+        snake_pather->positions[i] = snake_pather->positions[i - 1];
+    }
+    snake_pather->positions[0] = move_inside_grid(snake_pather->positions[0], dir, w);
+    --snake_pather->walk_this_way_counter;
+}
+
+static bool snake_pather_player_intersection(Snake_Pather *snake_pather, Player *player)
+{
+    for (Int i = 0; i < player->length; ++i)
+    {
+        for (Int j = 0; j < snake_pather->len; ++j)
+        {
+            if (pos_equal(snake_pather->positions[j], player_nth_position(player, i))) {
+				return true;
+			}
+        }
+    }
+	return false;
+}
+
 #define GAME_STATE_MAZE_FOODS 4
 #define GAME_STATE_MAZE_WIDTH 40 // you CANT change these, hardcoded
 #define GAME_STATE_MAZE_HEIGHT 30
+#define GAME_STATE_MAZE_TOTAL_EVIL_SNAKE_PATHS 2
 typedef struct
 {
     World_State0 w;
     Player player;
     double time_for_move;
     Food foods[GAME_STATE_MAZE_FOODS];
+    Snake_Pather evil_snake_paths[GAME_STATE_MAZE_TOTAL_EVIL_SNAKE_PATHS];
     Maze_Cell maze[GAME_STATE_MAZE_HEIGHT][GAME_STATE_MAZE_WIDTH];
 } Game_State_Maze;
 
@@ -593,20 +656,37 @@ static void game_state_Maze_init(Game_State_Maze *new_g)
     g.w = world_state0_init(GAME_STATE_MAZE_WIDTH);
     g.player = (Player){.length = 1, .idx_pos = 0, .current_direction = Dir_Nothing, .next_direction = Dir_Right};
     g.player.positions[0] = (Pos){.x = g.w.width / 2, g.w.height / 2};
-    // food_init_position(&g.food, &g.player, &g.w);
-    // printf("Size of Maze_Cell type: %lu\n", sizeof(Maze_Cell)); // this is SO stupid, why is the enum 4 bytes??!?!?!
-    // printf("Size of matrix: %lu\n", sizeof(g.maze));
-    const char *const maze_str = "|------------------------|---|---|------"
+
+    g.evil_snake_paths[0] = (Snake_Pather){
+        .len = 4,
+        .positions = {{29, 3}, {29, 4}, {29, 5}, {29, 6}},
+        .ways = {{Dir_Right, 2}, {Dir_Down, 6}, {Dir_Left, 2}, {Dir_Up, 6}},
+        .ways_len = 4,
+        .way_idx = 4, // be at end
+        .walk_this_way_counter = 0,
+    };
+    g.evil_snake_paths[1] = (Snake_Pather){
+        .len = 4,
+        .positions = {{12, 10}, {12, 11}, {12, 12}, {12, 13}},
+        .ways = {{Dir_Right, 14}, {Dir_Down, 7}, {Dir_Left, 14}, {Dir_Up, 7}},
+        .ways_len = 4,
+        .way_idx = 4, // be at end
+        .walk_this_way_counter = 0,
+    };
+
+    printf("Size of Maze_Cell type: %lu\n", sizeof(Maze_Cell)); // this is SO stupid, why is the enum 4 bytes??!?!?!
+    printf("Size of matrix: %lu\n", sizeof(g.maze));
+    const char *const maze_str = "----------------------------------------" // S does not do anything, just marker
                                  "----------------------------------------"
                                  "-|||||||------------------||||||||||||--"
-                                 "-|-----|------------------|----------|--"
+                                 "-|-----|------------------|--S-------|--"
                                  "-|-----|------------------|----------|--"
                                  "-|--F--|------------------|--------F-|--"
                                  "-|-----|------------------|----------|--"
                                  "-|-----|------------------|----------|--"
                                  "-|||--||----------|||||||||----------|--"
                                  "---|--|----||||||||------------------|--"
-                                 "---|--|----|----------------||||||||||--"
+                                 "---|--|----|S---------------||||||||||--"
                                  "---|--|----|-||||||-||||||--|--------|--"
                                  "---|--||||||-|-----------|-||--------|--"
                                  "---|---------|-----------|-|-----F---|--"
@@ -666,20 +746,32 @@ static Level_Return game_state_Maze_frame(Game_State_Maze *g)
 
     if (time_move_logic_general(&g->time_for_move, 0.123))
     {
-        if (player_move(&g->player, w))
+        Pos next_pos = move_inside_grid(player_nth_position(&g->player, 0), g->player.next_direction, w);
+        if (g->maze[next_pos.y][next_pos.x] != Maze_Cell_Wall) // check that the next position is not in a wall
         {
-            // player died
-            TraceLog(LOG_INFO, "%s", "YOU DIED!");
+            if (player_move(&g->player, w))
+            {
+                // player died
+                TraceLog(LOG_INFO, "%s", "YOU DIED!");
 
-            return Level_Return_Reset_Level;
+                return Level_Return_Reset_Level;
+            }
         }
-		for (Int i = 0; i < GAME_STATE_MAZE_FOODS; ++i) {
-			if (pos_equal(player_nth_position(&g->player, 0), g->foods[i].pos))
-			{
-				++g->player.length;
-				g->foods[i].pos = (Pos){.x = -1, .y = -1};
-			}
-		}
+        for (Int i = 0; i < GAME_STATE_MAZE_TOTAL_EVIL_SNAKE_PATHS; ++i)
+            snake_pather_move(&g->evil_snake_paths[i], w);
+
+        for (Int i = 0; i < GAME_STATE_MAZE_TOTAL_EVIL_SNAKE_PATHS; ++i)
+            if (snake_pather_player_intersection(&g->evil_snake_paths[i], &g->player))
+				return Level_Return_Reset_Level;
+
+        for (Int i = 0; i < GAME_STATE_MAZE_FOODS; ++i)
+        {
+            if (pos_equal(player_nth_position(&g->player, 0), g->foods[i].pos))
+            {
+                ++g->player.length;
+                g->foods[i].pos = (Pos){.x = -1, .y = -1};
+            }
+        }
     }
 
     Level_Return food_left_to_win = (DEV ? 5 : 5) - g->player.length;
@@ -694,9 +786,14 @@ static Level_Return game_state_Maze_frame(Game_State_Maze *g)
     draw_food_left(food_left_to_win);
     maze_draw((Maze_Cell *)g->maze, GAME_STATE_MAZE_WIDTH, GAME_STATE_MAZE_HEIGHT, w);
     player_draw_extra(&g->player, w);
-	for (Int i = 0; i < GAME_STATE_MAZE_FOODS; ++i) {
-		food_draw(&g->foods[i], w);
-	}
+    for (Int i = 0; i < GAME_STATE_MAZE_FOODS; ++i)
+    {
+        food_draw(&g->foods[i], w);
+    }
+    for (Int i = 0; i < GAME_STATE_MAZE_TOTAL_EVIL_SNAKE_PATHS; ++i)
+    {
+        snake_pather_draw(&g->evil_snake_paths[i], w);
+    }
 
     draw_fps();
     EndDrawing();
@@ -1072,10 +1169,9 @@ static Level_Return game_state1_frame0(Game_State1 *g)
             return Level_Return_Reset_Level;
         }
 
-        if (evil_snakes_player_collision_logic(g->evil_snakes, g->evil_snake_index, &g->player))
-        {
-            return Level_Return_Reset_Level;
-        }
+        for (Int i = 0; i < g->evil_snake_index; ++i)
+            if (evil_snake_player_collision_logic(g->evil_snakes, &g->player))
+                return Level_Return_Reset_Level;
 
         for (Int i = 0; i < g->evil_snake_index; ++i)
             evil_snake_move(&g->evil_snakes[i], w);
@@ -1140,7 +1236,8 @@ static Level_Return game_state1_frame0(Game_State1 *g)
     draw_food_left(food_left_to_win);
 
     player_draw(&g->player, w);
-    evil_snakes_draw(g->evil_snakes, g->evil_snake_index, w);
+    for (Int i = 0; i < g->evil_snake_index; ++i)
+        evil_snake_draw(&g->evil_snakes[i], w);
     food_draw(&g->food, w);
 
     draw_fps();
