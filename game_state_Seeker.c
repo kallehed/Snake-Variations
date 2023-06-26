7fbf8598f1fc5195efb227b1eb8ad54d7ad9f94e
kallehed <kallehed@gmail.com>
2023-06-26 22:24:17 +0200

    Done with Seeker level

--- game_state_Seeker.c
+++ game_state_Seeker.c
@@ -1,7 +1,8 @@
-
 #include "game_state_Seeker.h"
+#include "player_related.h"
+#include <stdlib.h>
 
-void game_state_Seeker_init(Game_State_Seeker *new_g)
+void game_state_init_Seeker(Game_State_Seeker *new_g)
 {
     Game_State_Seeker g;
     g.w = world_state0_init(28);
@@ -9,20 +10,25 @@ void game_state_Seeker_init(Game_State_Seeker *new_g)
     g.player.positions[0] = (Pos){.x = g.w.width / 2, g.w.height / 2};
     food_init_position(&g.food, &g.player, &g.w);
     g.time_for_move = 1.0;
-    g.evil_snake_index = 0;
+    g.seeker_time_for_move = 1.0;
+
+    g.seeker =
+        (Seeker_Snake){.length = 2, .direction = Dir_Left, .positions = {{.x = 26, .y = 10}, {.x = 27, .y = 10}}};
+    g.moves = 0;
 
     *new_g = g;
 }
 
-void evil_snake_move(Evil_Snake *snake, World_State0 *w)
+void seeker_move(Seeker_Snake *snake, World_State0 *w)
 {
-    for (int i = 0; i < snake->length; ++i)
+    for (int i = snake->length - 1; i >= 1; --i)
     {
-        snake->positions[i] = move_inside_grid(snake->positions[i], snake->direction, w);
+        snake->positions[i] = snake->positions[i - 1];
     }
+    snake->positions[0] = move_inside_grid(snake->positions[0], snake->direction, w);
 }
 
-void evil_snake_draw(const Evil_Snake *snake, const World_State0 *w)
+void seeker_draw(const Seeker_Snake *snake, const World_State0 *w)
 {
     const Int length = snake->length;
     for (Int j = 0; j < length; ++j)
@@ -32,7 +38,7 @@ void evil_snake_draw(const Evil_Snake *snake, const World_State0 *w)
 }
 
 // returns true on collision
-bool evil_snake_player_collision_logic(const Evil_Snake *snake, const Player *player)
+bool seeker_player_collision_logic(const Seeker_Snake *snake, const Player *player)
 {
     for (int i = 0; i < player->length; ++i)
     {
@@ -47,7 +53,7 @@ bool evil_snake_player_collision_logic(const Evil_Snake *snake, const Player *pl
     return false;
 }
 
-Level_Return game_state_Seeker_frame(Game_State_Seeker *g)
+Level_Return game_state_frame_Seeker(Game_State_Seeker *g)
 {
     World_State0 *w = &g->w;
     // logic
@@ -57,6 +63,56 @@ Level_Return game_state_Seeker_frame(Game_State_Seeker *g)
     }
 
     player_set_direction_from_input(&g->player);
+
+    bool seeker_grows = false;
+
+    if (time_move_logic_general(&g->seeker_time_for_move, 0.135894))
+    {
+        ++g->moves;
+        seeker_grows = (g->moves % 25 == 0) && (g->seeker.length < SEEKER_MAX_LENGTH);
+        if (seeker_grows)
+            g->seeker.length++;
+
+        {
+            Pos p_pos = player_nth_position(&g->player, 0);
+            Pos s_pos = g->seeker.positions[0];
+            Dir d;
+            if (abs(p_pos.x - s_pos.x) > abs(p_pos.y - s_pos.y))
+            {
+                d = ((p_pos.x - s_pos.x) < 0) ? Dir_Left : Dir_Right;
+            }
+            else
+            {
+                d = ((p_pos.y - s_pos.y) < 0) ? Dir_Up : Dir_Down;
+            }
+
+            Dir prev = g->seeker.direction;
+            if (prev == Dir_Right && d == Dir_Left)
+            {
+                d = Dir_Up;
+            }
+            else if (prev == Dir_Left && d == Dir_Right)
+            {
+                d = Dir_Up;
+            }
+            else if (prev == Dir_Up && d == Dir_Down)
+            {
+                d = Dir_Left;
+            }
+            else if (prev == Dir_Down && d == Dir_Up)
+            {
+                d = Dir_Left;
+            }
+            g->seeker.direction = d;
+        }
+
+        seeker_move(&g->seeker, w);
+
+        if (seeker_player_collision_logic(&g->seeker, &g->player))
+        {
+            return Level_Return_Reset_Level;
+        }
+    }
 
     if (time_move_logic(&g->time_for_move))
     {
@@ -68,64 +124,7 @@ Level_Return game_state_Seeker_frame(Game_State_Seeker *g)
             return Level_Return_Reset_Level;
         }
 
-        for (Int i = 0; i < g->evil_snake_index; ++i)
-        {
-            if (evil_snake_player_collision_logic(&g->evil_snakes[i], &g->player))
-            {
-                return Level_Return_Reset_Level;
-            }
-        }
-
-        for (Int i = 0; i < g->evil_snake_index; ++i)
-            evil_snake_move(&g->evil_snakes[i], w);
-
         food_player_collision_logic(&g->player, &g->food, w);
-
-        // spawn evil snakes
-        if (g->evil_snake_index < g->player.length - 1)
-        {
-            if (g->evil_snake_index < GAME_STATE1_TOTAL_EVIL_SNAKES)
-            {
-                // spawn
-                g->evil_snakes[g->evil_snake_index] = (Evil_Snake){.length = 2, Dir_Right, {{0, 5}, {1, 5}}};
-                Evil_Snake *snake = &g->evil_snakes[g->evil_snake_index];
-
-                Pos start;
-                Pos tail_dir;
-                Dir dir;
-
-                switch (GetRandomValue(1, 4))
-                {
-                case 1: // from left
-                    start = (Pos){.x = -1, .y = GetRandomValue(0, w->height - 1)};
-                    tail_dir = (Pos){-1, 0};
-                    dir = Dir_Right;
-                    break;
-                case 2: // from right
-                    start = (Pos){.x = w->width, .y = GetRandomValue(0, w->height - 1)};
-                    tail_dir = (Pos){1, 0};
-                    dir = Dir_Left;
-                    break;
-                case 3: // from top
-                    start = (Pos){.x = GetRandomValue(0, w->width - 1), .y = -1};
-                    tail_dir = (Pos){0, -1};
-                    dir = Dir_Down;
-                    break;
-                case 4: // from bottom
-                    start = (Pos){.x = GetRandomValue(0, w->width - 1), .y = w->height};
-                    tail_dir = (Pos){0, 1};
-                    dir = Dir_Up;
-                    break;
-                }
-                snake->length = GetRandomValue(2, EVIL_SNAKE_MAX_LENGTH);
-                for (Int i = 0; i < snake->length; ++i)
-                {
-                    snake->positions[i] = (Pos){start.x + tail_dir.x * i, start.y + tail_dir.y * i};
-                }
-                snake->direction = dir;
-                ++g->evil_snake_index;
-            }
-        }
     }
     Int food_left_to_win = (DEV ? 8 : 8) - g->player.length;
 
@@ -136,11 +135,15 @@ Level_Return game_state_Seeker_frame(Game_State_Seeker *g)
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
+    if (seeker_grows)
+    {
+        DrawRectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, RED);
+    }
+
     draw_food_left(food_left_to_win);
 
     player_draw(&g->player, w);
-    for (Int i = 0; i < g->evil_snake_index; ++i)
-        evil_snake_draw(&g->evil_snakes[i], w);
+    seeker_draw(&g->seeker, w);
     food_draw(&g->food, w);
 
     draw_fps();
--- game_state_Seeker.h
+++ game_state_Seeker.h
@@ -3,12 +3,12 @@
 #include "very_general.h"
 #include "player_related.h"
 
-#define EVIL_SNAKE_MAX_LENGTH 10
+#define SEEKER_MAX_LENGTH 10
 typedef struct 
 {
     Int length;
     Dir direction;
-    Pos positions[EVIL_SNAKE_MAX_LENGTH];
+    Pos positions[SEEKER_MAX_LENGTH];
 } Seeker_Snake;
 
 
@@ -19,14 +19,15 @@ typedef struct
     Player player;
     Food food;
     double time_for_move;
-    Int evil_snake_index;
+	double seeker_time_for_move;
     Seeker_Snake seeker;
+	Int moves;
 } Game_State_Seeker;
 
 void game_state_init_Seeker(Game_State_Seeker *new_g);
-void evil_snake_move(Evil_Snake *snake, World_State0 *w);
-void evil_snake_draw(const Evil_Snake *snake, const World_State0 *w);
+// void seeker_move(Seeker_Snake *snake, World_State0 *w);
+// void seeker_draw(const Seeker_Snake  *snake, const World_State0 *w);
 // returns true on collision
-bool evil_snake_player_collision_logic(const Evil_Snake *snake, const Player *player);
+// bool seeker_player_collision_logic(const Seeker_Snake  *snake, const Player *player);
 
 Level_Return game_state_frame_Seeker(Game_State_Seeker *g);
